4ccf5b47b28534f472a8f0a33bbfabb1a47ce473
nguyenxuan0411 <xuanntph21397@fpt.edu.vn>
2023-05-15 22:40:58 +0700

    user

---
+++ src/main/java/com/example/assm/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.assm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class UserController {
+    @RequestMapping("/index")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public String logForm(){
+        return "login";
+    }
+}
