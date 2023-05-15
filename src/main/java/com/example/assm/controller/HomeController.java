6915e90865f8ba43f66642da0b657786b97af7bf
thinhzzhb <dafuq412@gmail.com>
2023-05-15 17:25:49 +0700

    Update views

---
+++ src/main/java/com/example/assm/controller/HomeController.java
@@ -0,0 +1,57 @@
+package com.example.assm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/index")
+    public String home(){
+        return "index";
+    }
+    @GetMapping("/login")
+    public String logForm(){
+        return "login";
+    }
+    @GetMapping("/register")
+    public String regForm(){
+        return "register";
+    }
+    @GetMapping("/product")
+    public String showProd(){
+        return "shop";
+    }
+    @GetMapping("/order")
+    public String showOrder(){
+        return "order";
+    }
+    @GetMapping("/cart")
+    public String showCart(){
+        return "cart";
+    }
+    @GetMapping("/admin")
+    public String showAdmin(){
+        return "admin";
+    }
+    @GetMapping("/list_prod")
+    public String showlistProd(){
+        return "list_product";
+    }
+    @GetMapping("/man_acc")
+    public String editAcc(){
+        return "edit_account";
+    }
+    @GetMapping("/man_prod")
+    public String editProd(){
+        return "edit_prod";
+    }
+    @GetMapping("/list_acc")
+    public String showAcc(){
+        return "list_account";
+    }
+    @GetMapping("/man_stProd")
+    public String prodStatus(){
+        return "product_status";
+    }
+}
